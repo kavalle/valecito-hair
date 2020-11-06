12278d5a509c34360f298cb4a241528f3c18a5e0
Karina Valle <kar.valle@alumnos.duoc.cl>
2020-11-06 00:51:05 -0300

    configuraction de modela y tabla de administrador

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Reserva
+
+class ReservaAdmin(admin.ModelAdmin):
+    # readonly_fields=("hora")
+    list_display=["nombre_completo", "correo", "telefono", "hora"]
+    search_fields=["nombre_completo"]
+    list_filter=["nombre_completo","telefono"]
+
 
 # Register your models here.
+admin.site.register(Reserva, ReservaAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Reserva(models.Model):
+    nombre_completo= models.CharField(max_length=100)
+    correo = models.EmailField(max_length=100)
+    telefono = models.CharField(max_length=10)
+    servicio = models.CharField(max_length=50)
+    hora = models.TimeField()
