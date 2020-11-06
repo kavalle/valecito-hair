d8b491170c324e127fbc6557198a0a3c2a1f7404
Karina Valle <kar.valle@alumnos.duoc.cl>
2020-11-06 01:18:01 -0300

    configuration de paginas

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name='main'
 
 urlpatterns = [
-    path('', views.home, name='index')
+    path('', views.home, name='index'),
+    path('reservar', views.reservation, name='reservar')
 ]
--- main/views.py
+++ main/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 from valecitohair import views
 
 def home(request):
-    return render(request, "index.html")
+    return render(request, "inicio.html")
+
+def reservation(request):
+    return render(request, "reservar.html")
