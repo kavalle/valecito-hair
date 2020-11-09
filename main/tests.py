982f7ab76483dc4f529b4d570bd23872f68c9505
Javier Castro <sa.bachmann@alumnos.duoc.cl>
2020-11-08 22:58:53 -0300

    Add test units

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Reserva
 
-# Create your tests here.
+class BasicTest(TestCase):
+  def test_fields(self):
+      nueva_reserva = Reserva(
+        nombre_completo = 'Sady Bachmann',
+        correo = 'sa.bachmann@alumnos.duoc.cl',
+        telefono = '981648865',
+        servicio = 'Corte de pelo',
+        hora = '00:00'
+      )
+      nueva_reserva.save()
+      record = Reserva.objects.get(id=1)
+      self.assertEqual(record)
+
+  def test_error(self):
+      nueva_reserva = Reserva(
+        nombre_completo = 'Sady Bachmann',
+        correo = 'sa.bachmann@alumnos.duoc.cl',
+        telefono = '',
+        servicio = 'Corte de pelo',
+        hora = '00:00'
+      )
+      nueva_reserva.save()
+      record = Reserva.objects.get(id=1)
+      self.assertEqual(record)
